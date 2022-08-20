3770715b255924cc1d57ecd65e542dfc4e5f45ad
Anssi Varjonen <anssi.varjonen@sanoma.com>
2022-08-20 10:13:47 +0300

    Add project base

---
+++ src/credits.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Credits = () => {
+  return (
+    <div>
+      Test
+    </div>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Credits } from './credits';
+
+const root = document.getElementById('app');
+ReactDOM.render(<Credits />, root);
